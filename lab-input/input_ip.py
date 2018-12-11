e957f194faba252a37fa250134a20ef3ba433792
jonesant <anthony.jones@vzw.com>
2018-12-11 10:59:44 -0500

    Lad 8

---
+++ lab-input/input_ip.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+#Written by: AnthonyJ
+#Collect IP address information from a user, and display it back to them.
+## Collect input from the user
+user_input = input('Please enter an IPv4 address:')
+print("You told me the IPv4 address is:" + user_input)
+#This script will wait for input from the user to enter and it will comfirm to them what they entered.
+
---
+++ test.py
@@ -0,0 +1,12 @@
+# This is a test
+print("test Print")
+# This is the end of this test
+
+##################################
+# This is an important line!
+#################################
+
+
+
+
+
