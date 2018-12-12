0229a1f9fdc0d1b966dc451d17c57a287a863a30
jonesant <anthony.jones@vzw.com>
2018-12-12 15:14:19 -0500

    Lab 13

--- if-test/if-hostname.py
+++ if-test/if-hostname.py
@@ -2,7 +2,7 @@
 hostname = 'MTG'
 hostname = (input('Enter your hostname: '))
 
-if hostname == 'MTG':
+if hostname.upper() == 'MTG': # this allows any form of MTG to be accepted, MTG, mtg, MTg, mtG..
     print('The hostname was correct')
 print('Exiting the script')
      
